c64eda49ec85226e4f831edf8950cb869b7b8488
Phil Ewels <phil.ewels@scilifelab.se>
2017-12-20 16:41:43 +0100

    Moving everything into a subdirectory for cookiecutter.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,10 +3,6 @@
 #### Cookiecutter Post-Generation Script
 # Runs after the cookiecutter script has run
 
-# Remove the repo readme and replace with the cookiecutter readme
-rm ../README.md
-mv ../README-pipeline.md ../README.md
-
 # Delete the MIT licence file if not using the MIT licence
 if [ "${{ cookiecutter.mit_licence }}" != "y" ]; then     ## GOOD
     rm ../LICENCE
