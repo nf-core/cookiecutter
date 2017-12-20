a9b28144d8d8435faccbd03d90f0aa95bfd01f9f
Phil Ewels <phil.ewels@scilifelab.se>
2017-12-20 16:48:46 +0100

    Fixes from testing

--- hooks/post_gen_project.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin bash
-
-#### Cookiecutter Post-Generation Script
-# Runs after the cookiecutter script has run
-
-# Delete the MIT licence file if not using the MIT licence
-if [ "${{ cookiecutter.mit_licence }}" != "y" ]; then     ## GOOD
-    rm ../LICENCE
-fi
